f2b0329b9e622db79039dd9824c3bf6a7122e648
rkahne <rkahne@gmail.com>
2018-02-17 12:07:21 -0500

    Added naics to Brandon's Data

--- scripts/get_store_names.R
+++ scripts/get_store_names.R
@@ -4,12 +4,6 @@ library(magrittr)
 
 jeff_co <- read_excel('data/Jeff County Food Source Data.xlsx', sheet = 1)
 
-naics_code <- tribble(~code, ~definition,
-                      '445110', 'Grocery',
-                      '452910', 'Super Center and Club Store',
-                      '445120', 'Convenience Store',
-                      '447110', 'Food Mart',
-                      '445200', 'Specialized Lines of Food')
 jeff_co %<>% mutate(name_no_num = str_replace_all(Store_Name, '\\d', '') %>% 
                       str_replace_all('[:punct:]', '') %>% str_to_upper())
 
